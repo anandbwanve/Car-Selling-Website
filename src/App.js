72b4703efa9449879513a39faa61bf83a6249d89
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-17 12:31:59 +0530

    products

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import CarKharido from './assets/home.js';
 import EmailLogin from './assets/login';
+import Products from './assets/products';
 import Review from './assets/review';
 function App() {
   return(
     <>
-    <Review/>
+    <Products/>
     </>
   );
 }
