bb6ceffe902baf1f124887c6468a294a63379462
Abhinav Verma <abhinaverma1495@gmail.com>
2023-06-19 01:11:16 +0530

    v18-enquire

--- src/assets/enquire.js
+++ src/assets/enquire.js
@@ -1,50 +1,87 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import React from 'react';
-import logo from './logo.png';
-
+import { useRef, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+import React from "react";
+import logo from "./logo.png";
+import title from "./title.png";
+import "./login.css";
 
 function EnquireNow() {
-  const [contactNumber, setContactNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
+  let formRef = useRef();
+  let [isSuccess, setIsSuccess] = useState(false);
+  let [isError, setIsError] = useState(false);
 
-  const handleContactNumberChange = (e) => {
-    setContactNumber(e.target.value);
+  let [user, setUser] = useState({
+    contact: "",
+    email: "",
+    address: "",
+  });
+
+  let handlerContactAction = (e) => {
+    let newuser = { ...user, contact: e.target.value };
+    setUser(newuser);
+  };
+  let handlerEmailAction = (e) => {
+    let newuser = { ...user, email: e.target.value };
+    setUser(newuser);
+  };
+  let handlerAddressAction = (e) => {
+    let newuser = { ...user, address: e.target.value };
+    setUser(newuser);
   };
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+  let registerAction = async () => {
+    try {
+      formRef.current.classList.add("was-validated");
+      let formStatus = formRef.current.checkValidity();
+      if (!formStatus) {
+        return;
+      }
 
-  const handleAddressChange = (e) => {
-    setAddress(e.target.value);
-  };
+      // BACKEND
+      let url = `http://localhost:4000/adduser1?contact=${user.contact}&email=${user.email}&address=${user.address}`;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+      let res = await fetch(url);
 
-    // Process the form data (e.g., send it to a backend server)
-    // ...
+      if (res.status != 200) {
+        let serverMsg = await res.text();
+        throw new Error(serverMsg);
+      }
 
-    // Reset form fields after submission
-    setContactNumber('');
-    setEmail('');
-    setAddress('');
+      let newuser = {
+        contact: "",
+        email: "",
+        address: "",
+      };
+      setUser(newuser);
+
+      formRef.current.classList.remove("was-validated");
+
+      alert("success");
+      setIsSuccess(true);
+    } catch (err) {
+      alert(err.message);
+      setIsError(true);
+    } finally {
+      setTimeout(() => {
+        setIsSuccess(false);
+        setIsError(false);
+      }, 5000);
+    }
   };
 
   return (
-    <div>
-     {/* navigation */}
-     <section id="header">
+    <>
+      {/* navigation */}
+      <section id="header">
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top mb-1">
           <div className="container-fluid">
-          <Link to="/home" className="navbar-brand" >
+            <Link to="/home" className="navbar-brand">
               <img
                 src={logo}
                 alt="Logo"
                 className="img-fluid"
-                style={{ maxHeight: '35px' }}
+                style={{ maxHeight: "35px" }}
               />
             </Link>
             <button
@@ -61,95 +98,87 @@ function EnquireNow() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                <Link to="/home" className="nav-link active" aria-current="page" >Home</Link>
+                  <Link
+                    to="/home"
+                    className="nav-link active"
+                    aria-current="page"
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/login" className="nav-link" >Login</Link>
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/products" className="nav-link" >Products</Link>
+                  <Link to="/products" className="nav-link">
+                    Products
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/review" className="nav-link" >Reviews</Link>
+                  <Link to="/review" className="nav-link">
+                    Reviews
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/contact" className="nav-link"><b>Contact us</b></Link>
+                  <Link to="/contact" className="nav-link">
+                    <b>Contact us</b>
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
       </section>
-      {/* Enquire Now Form */}
-      <section
-        style={{
-         // backgroundImage: `url(${bgImage})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-        }}
-      >
-        <div className="container py-5">
-          <h4 className="mt-5 mb-5">
-            <strong>Enquire Now</strong>
-          </h4>
-          <div className="row justify-content-center">
-            <div className="col-md-6">
-              <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="contactNumber" className="form-label">
-                    Contact Number
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="contactNumber"
-                    value={contactNumber}
-                    onChange={handleContactNumberChange}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="address" className="form-label">
-                    Address
-                  </label>
-                  <textarea
-                    className="form-control"
-                    id="address"
-                    value={address}
-                    onChange={handleAddressChange}
-                    required
-                  ></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
 
-      {/* Footer */}
-      <div className="my-5">
-        <section>
-          {/* Footer code */}
-          {/* ... */}
-        </section>
+      {/* enquire */}
+
+      <div
+        className="row justify-content-center mt-10"
+        style={{ marginTop: "100px" }}
+      >
+        <div className="col-sm-12 col-md-6 ">
+          <div className="fs-2" style={{textAlign:"center"}}>Enquire Now</div>
+
+          <form ref={formRef} className="needs-validation">
+            <input
+              type="text"
+              className="form-control form-control-lg mb-2 mt-1"
+              placeholder="Enter Contact"
+              value={user.username}
+              onChange={handlerContactAction}
+              required
+            />
+            <input
+              type="email"
+              className="form-control form-control-lg mb-2"
+              placeholder="Enter Email"
+              value={user.email}
+              onChange={handlerEmailAction}
+              required
+            />
+            <input
+              type="text"
+              className="form-control form-control-lg mb-2"
+              placeholder="Enter address"
+              value={user.mobile}
+              onChange={handlerAddressAction}
+              required
+            />
+            <input
+              type="button"
+              value="Enquire"
+              className="w-100 btn btn-lg btn-secondary"
+              onClick={registerAction}
+            />
+          </form>
+
+          {isSuccess && <div className="alert alert-success">Success</div>}
+          {isError && <div className="alert alert-danger">Error</div>}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
